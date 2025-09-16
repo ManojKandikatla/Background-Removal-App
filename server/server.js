ffb6bd881ffa0f573f571cd57f3f30445d22834f
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-09-16 22:23:39 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
+
+//app config
+const PORT = process.env.PORT || 4000;
+const app = express()
+await connectDB()
+
+
+// initialize middlewares
+app.use(express.json())
+app.use(cors())
+
+//api routes
+app.get('/',(req,res)=>res.send("API is Working"))
+
+app.listen(PORT,()=>console.log("server running on port "+PORT)
+)
+
+
+
+
+//npm install express cors dotenv nodemon form-data jsonwebtoken mongoose multer axios svix razorpay these are the packages used in backend
